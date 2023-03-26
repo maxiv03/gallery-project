5cb599f1af53a6a01558c32a56a818a8753c1e30
maxiv03 <nocqponer04@gmail.com>
2023-03-26 06:01:09 -0300

    theme selector added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 $(document).ready(function(){
 
-    $(function(){
-        $('.bxslider').bxSlider({
-          mode: 'fade',
-          captions: true,
-          responsive: true,
-          pager: false
-        });
+  //slider function
+  $(function(){
+      $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        responsive: true,
+        pager: false
       });
+    });
+
+  //theme selector
+  const theme = $("#theme");
+  $("#to-theme-1").click(function(){
+    theme.attr("href", "css/theme-1.css");
+  });
+
+  $("#to-theme-2").click(function(){
+    theme.attr("href", "css/theme-2.css");
+  });
+
+  $("#to-theme-3").click(function(){
+    theme.attr("href", "css/theme-3.css");
+  });
+
+  $("#to-theme-4").click(function(){
+    theme.attr("href", "css/theme-4.css");
+  });
 
 });
