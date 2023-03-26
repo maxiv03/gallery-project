140d82b4a80d55fdd5d9655185c01b9590674ec8
maxiv03 <nocqponer04@gmail.com>
2023-03-26 06:28:14 -0300

    actual theme is now saved in localStorage

--- js/main.js
+++ js/main.js
@@ -12,20 +12,29 @@ $(document).ready(function(){
 
   //theme selector
   const theme = $("#theme");
+  let actualTheme = localStorage.getItem("actual-theme");
+  console.log(actualTheme);
+  if(actualTheme != null){
+    theme.attr("href", `css/${actualTheme}.css`);
+  }
   $("#to-theme-1").click(function(){
     theme.attr("href", "css/theme-1.css");
+    localStorage.setItem("actual-theme", "theme-1");
   });
 
   $("#to-theme-2").click(function(){
     theme.attr("href", "css/theme-2.css");
+    localStorage.setItem("actual-theme", "theme-2");
   });
 
   $("#to-theme-3").click(function(){
     theme.attr("href", "css/theme-3.css");
+    localStorage.setItem("actual-theme", "theme-3");
   });
 
   $("#to-theme-4").click(function(){
     theme.attr("href", "css/theme-4.css");
+    localStorage.setItem("actual-theme", "theme-4");
   });
 
 });
