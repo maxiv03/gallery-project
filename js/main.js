bbd350ed57be38898f6c99f3153b53fbfbdd2e4f
maxiv03 <nocqponer04@gmail.com>
2023-03-27 02:03:43 -0300

    added some animations for the theme selector, added one link to page start in the footer

--- js/main.js
+++ js/main.js
@@ -13,28 +13,59 @@ $(document).ready(function(){
   //theme selector
   const theme = $("#theme");
   let actualTheme = localStorage.getItem("actual-theme");
-  console.log(actualTheme);
   if(actualTheme != null){
     theme.attr("href", `css/${actualTheme}.css`);
   }
+
   $("#to-theme-1").click(function(){
-    theme.attr("href", "css/theme-1.css");
+
+    $('html, body').fadeOut(500, function(){
+      theme.attr("href", "css/theme-1.css");
+    });
+    $('html, body').fadeIn();
+
     localStorage.setItem("actual-theme", "theme-1");
   });
 
   $("#to-theme-2").click(function(){
-    theme.attr("href", "css/theme-2.css");
+
+    $('html, body').fadeOut(500, function(){
+      theme.attr("href", "css/theme-2.css");
+    });
+    $('html, body').fadeIn();
+
     localStorage.setItem("actual-theme", "theme-2");
   });
 
   $("#to-theme-3").click(function(){
-    theme.attr("href", "css/theme-3.css");
+
+    $('html, body').fadeOut(500, function(){
+      theme.attr("href", "css/theme-3.css");
+    });
+    $('html, body').fadeIn();
+
     localStorage.setItem("actual-theme", "theme-3");
   });
 
   $("#to-theme-4").click(function(){
-    theme.attr("href", "css/theme-4.css");
+    
+    $('html, body').fadeOut(500, function(){
+      theme.attr("href", "css/theme-4.css");
+    });
+    $('html, body').fadeIn();
+
     localStorage.setItem("actual-theme", "theme-4");
+  });
+
+  //Scroll to the start of the page
+  $(".to-start").click(function(e){
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500)
+
+    return false;
   });
 
 });
